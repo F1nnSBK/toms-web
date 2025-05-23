ad712008b577d5ef79edd58e070cabc86c414fd7
Finn <fhertsch235@gmail.com>
2025-05-23 18:21:15 +0200

    init

---
+++ app/src/test/java/com/toms/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toms.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
